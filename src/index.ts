50b274538ec1b1e539858b7107f120da3b2f4c30
aereal <aereal@aereal.org>
2019-07-24 12:43:45 +0900

    aws-cdk 0.31.0

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ const asset: FileAssetMetadataEntry = {
   path: './src/artifacts/',
   id: 'artifact-test',
   packaging: 'zip',
-  sourceHash: 'bc86a4e3b402ebc7d00d4aeae2250806570262a614bf05f207e96bc60a5e41c1', // TODO
+  // sourceHash: 'bc86a4e3b402ebc7d00d4aeae2250806570262a614bf05f207e96bc60a5e41c1', // TODO
   s3BucketParameter: '<ignored>',
   s3KeyParameter: '<ignored>',
-  artifactHashParameter: '<ignored>',
+  // artifactHashParameter: '<ignored>',
 };
 
 const main = async () => {
